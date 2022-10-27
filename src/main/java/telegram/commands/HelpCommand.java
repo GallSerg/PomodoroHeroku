a7b02768a135f5c511616bbee389fb91a53625bb
GalSerg <warlik.galchin@yandex.ru>
2022-10-27 21:07:24 +0500

    initial commit

---
+++ src/main/java/telegram/commands/HelpCommand.java
@@ -0,0 +1,21 @@
+package telegram.commands;
+
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+public class HelpCommand extends ServiceCommand {
+
+    public HelpCommand(String commandIdentifier, String description) {
+        super(commandIdentifier, description);
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+        sendAnswer(absSender, chat.getId(),
+                "Привет, я тайм-менеджер Pomodoro.\n" +
+                        "Можете воспользоваться командами /begin для начала работы.\n" +
+                        "После начала работы необходимо ввести количество ваших задач (число)");
+
+    }
+}
